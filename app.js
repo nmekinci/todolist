99d9305c60bfab23a805ffc8773aa3cdff41ca4c
Must <emailme>
2023-06-09 16:19:55 +0200

    cont

--- app.js
+++ app.js
@@ -4,25 +4,43 @@ let addButton = document.getElementById("add-button");
 let showTask = document.querySelector(".container");
 
 // *********** Events ***********
-addButton.addEventListener("click", (e) => {
+addButton.addEventListener("click", () => {
     let userText = userInput.value;
     showTask.appendChild(createDiv(userText));
-    console.log(userText);
-    let checkbox = document.createElement("input");
+    // console.log(userText);
+    // let checkbox = document.createElement("input");
+    userInput.value = ""
 })
+showTask.addEventListener("click", (e) =>{
+    console.log(e.target.className);
+    
+    
+
+    if (e.target.type === "checkbox"){
+        console.log(e.y);
+        let lineP = document.getElementsByClassName("pp")
+        
+      
+        console.log(lineP);
+    }
+})
+
+// *********** functions ***********
+
+
 function createDiv(userTextParameter){
     let newDiv = document.createElement("div");
     let newDivChild = document.createElement("div");
     let newInput = document.createElement("input");
     let newP = document.createElement("p");
-    newDiv.className = "input-group mb-3";
+    newDiv.className = "input-group mb-3 bg-light";
     newDivChild.className = "input-group-text";
     newInput.className = "form-check-input mt-0";
     newInput.type = "checkbox";
     newP.innerText = userTextParameter;
+    newP.className = "m-auto pp"
     newDivChild.appendChild(newInput);
     newDiv.appendChild(newDivChild);
     newDiv.appendChild(newP);
-    newDiv.app
     return newDiv;
 }
